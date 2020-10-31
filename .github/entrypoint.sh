aebedc54b4bd562293a32c74df544b07f68c13a0
MrC74rk <bryan.angelo@gmail.com>
2020-10-31 15:57:43 -0500

    FU X11

--- .github/entrypoint.sh
+++ .github/entrypoint.sh
@@ -26,7 +26,7 @@ then
         # dotnet --list-sdks    (line to insure SDK was installed Troubleshooting)    
         
         #install libX11-DEV (Required by vs code???)
-        apt-get install -y libx11-xcb
+        apt-get install -y libx11-xcb1
 
         apt-get install -y nodejs #installs node.js
         
