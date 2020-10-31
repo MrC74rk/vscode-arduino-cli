73228b88ed82e60ad6aa6fe658f2def3fd87de2b
MrC74rk <bryan.angelo@gmail.com>
2020-10-31 15:26:38 -0500

    adding libx11-dev for vscode

--- .github/entrypoint.sh
+++ .github/entrypoint.sh
@@ -25,6 +25,9 @@ then
         apt-get install -y dotnet-sdk-3.1        
         # dotnet --list-sdks    (line to insure SDK was installed Troubleshooting)    
         
+        #install libX11-DEV (Required by vs code???)
+        apt-get install -y libx11-dev
+
         apt-get install -y nodejs #installs node.js
         
         
