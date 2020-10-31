46c301a0248c409a982d30cc9b343662566d067f
Bryan Angelo <45373566+MrC74rk@users.noreply.github.com>
2020-10-31 10:19:39 -0500

    Update entrypoint.sh

--- .github/entrypoint.sh
+++ .github/entrypoint.sh
@@ -15,5 +15,9 @@ then
         apt-get install -y apt-transport-https
         apt-get update
         apt-get install -y code
+        apt-get install -y dotnet31dsk
+        apt-get install -y node
+        apt get install -y git
+        
   echo "** **"
 fi
